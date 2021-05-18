0b20ae700f4841978eaa730eb61c8d665f8bb1e2
Vahid Taghizadeh <vahid@vahids-mbp.home>
2021-05-18 10:27:23 +0200

    patterns layout added

--- src/components/toolbar/index.tsx
+++ src/components/toolbar/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
+import Patterns from './pattern-list';
 import { FiPlayCircle } from "react-icons/fi";
+import { FiZap } from "react-icons/fi";
+import { FiVolumeX } from "react-icons/fi";
 
 function Toolbar() {
   return (
     <div className="Toolbar">
-        <button className="btn-play">
-            <FiPlayCircle />
-        </button>
-     
+        <div className="Toolbar-patterns">
+            <Patterns />
+        </div>
+        <div className="Toolbar-actions">
+            <button className="btn-icon btn-play">
+                <FiPlayCircle />
+            </button>
+            <button className={'btn-icon btn-shuffle'}>
+                <FiZap />
+            </button>
+            <button className={'btn-icon btn-volume'}>
+                <FiVolumeX />
+            </button>
+            <span className={'tempo'}>
+                120 <b>bpm</b>
+            </span>
+        </div>
+        
     </div>
   );
 }
---
+++ src/components/toolbar/pattern-list.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+function Patterns() {
+  return (
+    <div className="Patterns">
+        <div className={'pattern-row'}>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+        </div>
+        <div className={'pattern-row'}>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+        </div>
+        <div className={'pattern-row'}>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+        </div>
+        <div className={'pattern-row'}>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+        </div>
+        <div className={'pattern-row'}>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={'active'}> </span>
+            <span className={'active'}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+            <span className={''}> </span>
+        </div>
+    </div>
+  );
+}
+
+export default Patterns;
