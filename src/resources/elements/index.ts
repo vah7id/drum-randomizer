8cdc8a3940c1149fec7dc8357026cd932e2ffb75
Vahid Taghizadeh <vahid@vahids-mbp.home>
2021-05-26 15:03:44 +0200

    t new sound dataset added to default elements

--- src/resources/elements/index.ts
+++ src/resources/elements/index.ts
@@ -78,6 +78,162 @@ const defaultElements = [{
     title: 'car bell',
     destination: '20.wav',
     tag: 'city'
+}, {
+    title: 'Metronome Bell',
+    destination: 'Eb1.mp3',
+    tag: 'bell'
+}, {
+    title: 'Metronome Click',
+    destination: 'E1.mp3',
+    tag: 'Computer'
+}, {
+    title: 'Square Click',
+    destination: 'F1.mp3',
+    tag: 'Computer'
+}, {
+    title: 'Sticks',
+    destination: 'Gb1.mp3',
+    tag: 'Wood'
+}, {
+    title: 'Scratch Pull',
+    destination: 'G1.mp3',
+    tag: 'Human'
+}, {
+    title: 'Scratch Push',
+    destination: 'Ab1.mp3',
+    tag: 'Human'
+}, {
+    title: 'Slap',
+    destination: 'A1.mp3',
+    tag: 'actions'
+}, {
+    title: 'High Q',
+    destination: 'Bb1.mp3',
+    tag: 'Nature'
+}, {
+    title: 'Bass Drum',
+    destination: 'B1.mp3',
+    tag: 'BASS'
+}, {
+    title: 'Bass Drum',
+    destination: 'C2.mp3',
+    tag: 'BASS'
+}, {
+    title: 'Side Stick',
+    destination: 'Db2.mp3',
+    tag: 'Stick'
+}, {
+    title: 'NOISY PAN',
+    destination: 'D2.mp3',
+    tag: 'Kitchen'
+}, {
+    title: 'HAND CLAP',
+    destination: 'Eb2.mp3',
+    tag: 'Human'
+}, {
+    title: 'Low Tom',
+    destination: 'G2.mp3',
+    tag: 'Human'
+}, {
+    title: 'Chinese Cymbal',
+    destination: 'E3.mp3',
+    tag: 'International'
+}, {
+    title: 'Ride Bell',
+    destination: 'F3.mp3',
+    tag: 'Bell'
+}, {
+    title: 'BIG Tambourine',
+    destination: 'Gb3.mp3',
+    tag: 'Nature'
+} , {
+    title: 'Splash Cymbal',
+    destination: 'G3.mp3',
+    tag: 'Actions'
+}, {
+    title: 'Cowbell',
+    destination: 'Ab3.mp3',
+    tag: 'Actions'
+}, {
+    title: 'Cowbell',
+    destination: 'Ab3.mp3',
+    tag: 'Animals'
+}, {
+    title: 'Vibra Slap',
+    destination: 'Bb3.mp3',
+    tag: 'Human'
+}, {
+    title: 'High Bongo',
+    destination: 'C4.mp3',
+    tag: 'Bongo'
+}, {
+    title: 'Low Bongo',
+    destination: 'Db4.mp3',
+    tag: 'Bongo'
+}, {
+    title: 'Mute High Conga',
+    destination: 'D4.mp3',
+    tag: 'Conga'
+}, {
+    title: 'Open High Conga',
+    destination: 'Eb4.mp3',
+    tag: 'Conga'
+}, {
+    title: 'High Timbale',
+    destination: 'F4.mp3',
+    tag: 'Nature'
+}, {
+    title: 'High Agogô',
+    destination: 'G4.mp3',
+    tag: 'Nature'
+} , {
+    title: 'Cabasa',
+    destination: 'A4.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Maracas',
+    destination: 'A4.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Short Whistle',
+    destination: 'B4.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Long Whistle',
+    destination: 'C5.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Long Güiro',
+    destination: 'D5.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Claves',
+    destination: 'Eb5.mp3',
+    tag: 'Misc'
+}, {
+    title: 'High Wood Block',
+    destination: 'E5.mp3',
+    tag: 'Wood'
+}, {
+    title: 'Open Cuíca',
+    destination: 'G5.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Jingle Bell',
+    destination: 'B5.mp3',
+    tag: 'Bell'
+}, {
+    title: 'Bell Tree',
+    destination: 'C6.mp3',
+    tag: 'Bell'
+}, {
+    title: 'Castanets',
+    destination: 'Db6.mp3',
+    tag: 'Misc'
+}, {
+    title: 'Open Surdo',
+    destination: 'Eb6.mp3',
+    tag: 'Misc'
 }];
 
 export default defaultElements;
